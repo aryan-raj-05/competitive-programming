c7e22e4ba8b9aca608bfad8aa46c35a0cfc61d98
aryan-raj-05 <161190212+aryan-raj-05@users.noreply.github.com>
2025-03-06 11:24:57 +0530

    solved problem palindrome reorder

--- CSES/palindrome_reorder.cpp
+++ CSES/palindrome_reorder.cpp
@@ -1,18 +1,34 @@
+/*
+
+Given a string, your task is to reorder its letters in such a way that it becomes a palindrome 
+(i.e., it reads the same forwards and backwards).
+
+Input: The only input line has a string of length n consisting of characters A–Z.
+Output: Print a palindrome consisting of the characters of the original string. 
+You may print any valid solution. If there are no solutions, print "NO SOLUTION".
+
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-void solve(string s) {
+void solve(string& s) {
+    vector<int> mp(26, 0);
     int len = s.size();
-    int map[26];
+
     for (auto ch : s) {
-        map[ch - 'A']++;
+        mp[ch - 'A']++;
     }
 
     int cnt = 0;
-    for (auto val : map) {
-        if (val % 2 != 0)   
+    char oddChar = 0;
+    for (int i = 0; i < 26; ++i) {
+        int val = mp[i];
+        if (val % 2 != 0) {
             ++cnt;
+            oddChar = 'A' + i;
+        }
     }
 
     if (cnt > 1) {
@@ -20,8 +36,21 @@ void solve(string s) {
         return;
     }
 
-    string s(len, ' ');
-    
+    int i = 0, j = len - 1;
+
+    for (int iter = 0; iter < 26; ++iter) {
+        while (mp[iter] > 1) {
+            s[i] = s[j] = 'A' + iter;
+            ++i; --j;
+            mp[iter] -= 2;
+        }
+    }
+
+    if (cnt == 1) {
+        s[i] = oddChar;
+    }
+
+    cout << s << "\n";
 }
 
 int main() {
