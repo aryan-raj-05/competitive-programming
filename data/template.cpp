076cd613fa077b6900f7cdf116b37288dd095142
aryan-raj-05 <161190212+aryan-raj-05@users.noreply.github.com>
2025-03-09 09:20:36 +0530

    rearranged some files

--- data/template.cpp
+++ data/template.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 
 using ll = long long;
+using pii = pair<int,int>;
 using vi = vector<int>;
-using pi = pair<int,int>;
 
-#define all(x) begin(x), end(x)
+#define all(x) x.begin(), x.end()
+#define FOR(i, a, b) for(int i = a; i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
 #define sz(x) (int) (x).size()
+#define each(a, x) for(auto& a : x)
+
 #define pb push_back
 #define f first
 #define s second
 #define mp make_pair
-
-#define rep(i, n) for (int i = 0; i < (n); ++i)
 
 void setIO(string name = "") {
     cin.tie(0)->sync_with_stdio(0);
@@ -23,5 +25,6 @@ void setIO(string name = "") {
 }
 
 int main() {
-    setIO(); 
+    setIO();
+    
 }
