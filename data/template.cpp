fb33ffc0d8faaca4695bc9f10aca38e971248d67
aryan-raj-05 <161190212+aryan-raj-05@users.noreply.github.com>
2025-04-10 22:08:10 +0530

    fadfad

--- data/template.cpp
+++ data/template.cpp
@@ -1,28 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifndef ONLINE_JUDGE
-#include "../data/debugging.h"
-#define log(x) cout << #x << " = " << x << '\n'
-#else
-#define log(x)
-#endif
-
 using ll = long long;
 using pii = pair<int,int>;
-using str = string;
 using vi = vector<int>;
 
 #define all(x)          x.begin(), x.end()
 #define iter(a, x)      for (auto& a : x)
 #define len(x)          (int) (x).size()
-#define range(i, a, b)  for (int i = a; i < (b); ++i)
-#define rep(i, n)       for (int i = 0; i < (n); ++i)
-
-#define mp make_pair
-#define pb push_back
-#define ff first
-#define ss second
+#define rep(i, a, b)    for (int i = a; i < (b); i++)
 
 void setIO(string name = "") {
 	cin.tie(0)->sync_with_stdio(0); 
@@ -33,5 +19,5 @@ void setIO(string name = "") {
 }
 
 int main() {
-
+	
 }
