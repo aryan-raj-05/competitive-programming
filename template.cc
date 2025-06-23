2992012e7a11de10d727829619425f3384769fc2
aryan-raj-05 <161190212+aryan-raj-05@users.noreply.github.com>
2025-06-23 10:24:16 +0530

    added utility files

---
+++ template.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define len(x) (int)(x).size()
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define trav(i, x) for(auto& i : x)
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	
+}
